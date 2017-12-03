53dbb0191867b2b3df446c3a22bd2799fad4174d
woqs <durrieu.florian@gmail.com>
2017-12-03 19:47:45 +0100

    refacto: clean up

--- run.py
+++ run.py
@@ -9,32 +9,35 @@ localIdPath = dir_path +'/'+ localFileName
 localAdressPath = dir_path +'/'+ adressFileName
 driveFileInformations = {'title': 'CurrentIP'}
 
+
+def writeNewIP(filename, ip):
+    file = open(filename, 'w')
+    file.write(ip)
+    file.close()
+
 def hasSameIp(filename, ip):
     if os.path.exists(filename):
         localFile = open(filename, 'r')
         lastIp = localFile.read()
         localFile.close()
         if lastIp != ip :
-            localFile = open(filename, 'w')
-            localFile.write(ip)
-            localFile.close()
+            writeNewIP(filename, ip)
             return False
         else:
             return True
-    localFile = open(filename, 'w')
-    localFile.write(ip)
-    localFile.close()
+    writeNewIP(filename, ip)
     return False
 
 
-
 currentIp = urllib2.urlopen('http://ip.42.pl/raw').read()
+print "Current IP : %s" % currentIp
 
 if hasSameIp(localAdressPath, currentIp):
     sys.exit(0)
 
 gauth = GoogleAuth()
 gauth.LocalWebserverAuth()
+print "Auth to Google"
 
 drive = GoogleDrive(gauth)
 
@@ -46,7 +49,7 @@ if os.path.exists(localIdPath):
 fileToUpdate = drive.CreateFile(driveFileInformations)
 fileToUpdate.SetContentString(currentIp)
 fileToUpdate.Upload()
-
+print "File Uploaded"
 if not os.path.exists(localIdPath):
     localFile = open(localIdPath, 'w')
     localFile.write(fileToUpdate['id'])
